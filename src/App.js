4db84a31314bd2b2d600ee7cb12db1f862f61745
sufyanarain <sufyan822@gmail.com>
2021-12-03 18:51:44 +0500

    tried to used 'context api for auth'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import Routes from './routes/Routes';
+import logo from "./logo.svg";
+import "./App.css";
+import Routes from "./routes/Routes";
+import AuthContext from "./components/context/AuthContext";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useState } from "react";
+import { auth } from "./components/firebase";
 
 function App() {
+  const [firebseAuth, setFirebaseAuth] = useState({ isLoggedIn: false });
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setFirebaseAuth({ isLoggedIn: true });
+      console.log(user);
+      const uid = user.uid;
+      // ...
+    }
+  });
+
   return (
-    
-    <Routes />
+    <AuthContext.Provider value={firebseAuth}>
+      <Routes />
+    </AuthContext.Provider>
   );
 }
 
--- src/components/firebase.js
+++ src/components/firebase.js
@@ -17,6 +17,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const stateChange = onAuthStateChanged;
-const storage = getStorage(app);
+// const storage = getStorage(app);
 
-export { firebaseConfig, auth, db,storage, stateChange }
+export { firebaseConfig, auth, db, stateChange }
