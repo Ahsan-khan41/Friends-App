94d3879e6117946fd5ee82ace067eca5d098c6e2
sufyan arain <sufyan822@gmail.com>
2021-12-10 02:16:40 +0500

    fixed a bug

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import RoutesLoggedIn from "./routes/RoutesLoggedIn.js";
 import RoutesNotLoggedIn from "./routes/RoutesNotLoggedIn";
 import { onAuthStateChanged } from "firebase/auth";
 import { useState, useEffect } from "react";
-import { auth } from "./components/firebase";
+import { auth, firebaseConfig } from "./components/firebase";
 import CurentUserContext from "./components/context/CurrentUserContext";
 
 function App() {
   const [firebaseAuth, setFirebaseAuth] = useState(false);
   const [currentUser, setCurrentUser] = useState({})
- 
 
   useEffect(() => {
 
     onAuthStateChanged(auth, (user) => {
-      setFirebaseAuth(true);
-      setCurrentUser(user)
+      if (user) {
+        setFirebaseAuth(true);
+        setCurrentUser(user)
+
+      }
 
     });
 
