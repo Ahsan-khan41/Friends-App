5206d85bf4435bc7cb8c549844d2ec148a92bba1
sufyanarain <sufyan822@gmail.com>
2021-12-17 21:41:26 +0500

    applied background color on home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,65 +1,58 @@
-import React, { useEffect, useContext, useState } from 'react'
-import Nav from '../../components/Nav/Nav'
-import { Button, Input, Divider } from 'antd'
-import UserCard from './userCard/UserCard'
-import { CreditCardOutlined, VideoCameraAddOutlined } from '@ant-design/icons';
-import './userCard/userCard.css'
+import React, { useEffect, useContext, useState } from "react";
+import Nav from "../../components/Nav/Nav";
+import { Button, Input, Divider } from "antd";
+import UserCard from "./userCard/UserCard";
+import { CreditCardOutlined, VideoCameraAddOutlined } from "@ant-design/icons";
+import "./userCard/userCard.css";
 import { doc, onSnapshot } from "firebase/firestore";
-import { db } from '../../components/firebase';
-import CurentUserContext from '../../components/context/CurrentUserContext';
-import './home.css'
-import Posts from '../posts/Posts';
-
-
-
+import { db } from "../../components/firebase";
+import CurentUserContext from "../../components/context/CurrentUserContext";
+import "./home.css";
+import Posts from "../posts/Posts";
 
 const Home = () => {
-    const userObj = useContext(CurentUserContext)
-    // let userObj = localStorage.getItem('user')
-    // userObj = JSON.parse(userObj)
+  const userObj = useContext(CurentUserContext);
+  // let userObj = localStorage.getItem('user')
+  // userObj = JSON.parse(userObj)
 
+  const [getUsers, setGetusers] = useState({});
 
-    const [getUsers, setGetusers] = useState({})
+  useEffect(() => {
+    onSnapshot(doc(db, "users", `${userObj.uid}`), (doc) => {
+      if (doc.data()) {
+        setGetusers(doc.data());
+        // console.log("Current data: ", doc.data())
+      }
+    });
+  }, [userObj]);
 
+  return (
+    // <div>
+    //     {/* <div className='userCard'>
+    //         <UserCard name={getUsers.name} email={getUsers.email} img={getUsers.img} />
 
-    useEffect(() => {
-        onSnapshot(doc(db, "users", `${userObj.uid}`), (doc) => {
-            if (doc.data()) {
-                setGetusers(doc.data())
-                // console.log("Current data: ", doc.data())
+    //     </div> */}
+    // </div>
+    <div>
+      <Nav />
 
-            }
-        });
-
-    }, [userObj])
-
-
-
-
-
-    return (
-        // <div>
-        //     {/* <div className='userCard'>
-        //         <UserCard name={getUsers.name} email={getUsers.email} img={getUsers.img} />
-
-        //     </div> */}
-        // </div>
-        <div>
-            <Nav />
-
-            <div className='create-post-div'>
-                <Input placeholder="What's on your mind ?" className='post-input' />
-                <Divider />
-                <div className='buttons-div' >
-                    <Button type="primary"><CreditCardOutlined /> Photo</Button>
-                    <Button type="primary"><VideoCameraAddOutlined /> Video</Button>
-                </div>
-            </div>
-            <div className='post-div'>
-                <Posts/>
-            </div>
+      <div className="post-div">
+        <div className="create-post-div">
+          <Input placeholder="What's on your mind ?" className="post-input" />
+          <Divider />
+          <div className="buttons-div">
+            <Button type="primary">
+              <CreditCardOutlined /> Photo
+            </Button>
+            <Button type="primary">
+              <VideoCameraAddOutlined /> Video
+            </Button>
+          </div>
         </div>
-    )
-}
+        <Posts />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/pages/userPage/UserPage.js
+++ src/pages/userPage/UserPage.js
@@ -1,55 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import { Image } from 'antd';
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Image } from "antd";
+import { useParams } from "react-router-dom";
 
-import './userPage.css'
+import "./userPage.css";
 import { collection, query, where, onSnapshot } from "firebase/firestore";
-import { db } from '../../components/firebase';
-import Nav from '../../components/Nav/Nav'
-import { Typography } from 'antd';
-import UserTabs from './userTabs/UserTabs';
+import { db } from "../../components/firebase";
+import Nav from "../../components/Nav/Nav";
+import { Typography } from "antd";
+import UserTabs from "./userTabs/UserTabs";
 const { Title } = Typography;
 
 const UserPage = () => {
-    let params = useParams();
-    console.log(params)
-    const [userPeram, setUserPeram] = useState(params)
-    // console.log(userPeram);
-    useEffect(() => {
-        const q = query(collection(db, "users"), where("uid", "==", `${params.user}`));
-        onSnapshot(q, (querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                setUserPeram(doc.data());
-            });
+  let params = useParams();
+  console.log(params);
+  const [userPeram, setUserPeram] = useState(params);
+  // console.log(userPeram);
+  useEffect(() => {
+    const q = query(
+      collection(db, "users"),
+      where("uid", "==", `${params.user}`)
+    );
+    onSnapshot(q, (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        setUserPeram(doc.data());
+      });
+    });
+  }, [params]);
 
-        });
-    }, [params])
-
-    return (
-        <div>
-            <Nav />
-            <div className='parentDiv'>
-                {/* <Row>
-                    <Col span={16} offset={4}> */}
-                <Image className='background-img'
-                    width={'100%'}
-                    src={userPeram.img}
-                />
-                <div className='profile-div'>
-                    <Image preview={false} className='profile-img'
-                        src="https://media.istockphoto.com/photos/smiling-indian-business-man-working-on-laptop-at-home-office-young-picture-id1307615661"
-                    />
-                    <Title className='user-name' level={1}>{userPeram.name}</Title>
-                </div>
-                {/* <Title className='emailHead' level={5}>Sufyan@gmail.com</Title> */}
-                {/* <hr /> */}
-
-                {/* </Col>
-                </Row> */}
-                <UserTabs user={userPeram} />
-            </div>
+  return (
+    <div>
+      <Nav />
+      <div className="parentDiv">
+        <Image className="background-img" width={"100%"} src={userPeram.img} />
+        <div className="profile-div">
+          <Image
+          style={{width:100}}
+            preview={false}
+            className="profile-img"
+            src="https://media.istockphoto.com/photos/smiling-indian-business-man-working-on-laptop-at-home-office-young-picture-id1307615661"
+          />
         </div>
-    )
-}
+          <Title className="user-name" level={1}>
+            {userPeram.name}
+          </Title>
 
-export default UserPage
+        <UserTabs user={userPeram} />
+      </div>
+    </div>
+  );
+};
+
+export default UserPage;
